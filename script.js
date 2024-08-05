64c7a5e725a3c5ce6af04302a45fc535de75c805
fajarputragumilang <fajarputragumilangpg@gmail.com>
2024-08-05 09:08:45 +0700

    login page

---
+++ script.js
@@ -0,0 +1,6 @@
+const darkmode = document.getElementById('darkmode')
+
+darkmode.style.width = '90px';
+darkmode.style.backgroundColor = 'blue';
+darkmode.style.height = '80px';
+darkmode.style.color = 'red'
